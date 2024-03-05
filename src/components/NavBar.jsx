95be81859aef90a4dfdd9fcb730db64a24284223
Fred <pf.robert974@gmail.com>
2024-03-05 17:30:26 +1100

    make the navBar more modern looking

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,27 +12,43 @@ function Navbar() {
 
   const isMapView = pathMatchRoute("/map");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            {isMapView && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  <ListIcon className="icon" alt="List" /> <span>List</span>
-                </Link>
-              </li>
-            )}
-            {!isMapView && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/map">
-                  <MapIcon className="icon" alt="Map" /> <span>Map</span>
-                </Link>
-              </li>
-            )}
-          </ul>
+        <div className="navbar-nav">
+          <form
+            className="d-flex me-auto"
+            role="search"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search gig, ..."
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
+        <li className="nav-item">
+          {isMapView ? (
+            <Link className="nav-link" to="/">
+              <ListIcon className="icon" alt="List" />
+              <span>List</span>
+            </Link>
+          ) : (
+            <Link className="nav-link" to="/map">
+              <MapIcon className="icon" alt="Map" />
+              <span>Map</span>
+            </Link>
+          )}
+        </li>
       </div>
     </nav>
   );
