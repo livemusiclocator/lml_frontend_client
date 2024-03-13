5632caa4bbbc167d83c8eb7596946a81ced5b98f
Fred <pf.robert974@gmail.com>
2024-03-13 15:16:47 +1100

    refactor SearchForm

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useSearch } from "../contexts/SearchContext";
 import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import "react-datepicker/dist/react-datepicker.css";
 
 function SearchForm() {
@@ -13,18 +14,15 @@ function SearchForm() {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
+    <Form onSubmit={handleSubmit} inline>
+      <InputGroup>
         <Form.Control
           type="text"
           value={postcode}
           onChange={(e) => setPostcode(e.target.value)}
           placeholder="Search gig, ..."
         />
-      </Form.Group>
-      <Form.Group>
-        <Form.Control type="submit" value="Search" />
-      </Form.Group>
+      </InputGroup>
     </Form>
   );
 }
