2391018dc463de8352bfac6a934232af8a83712d
Fred <pf.robert974@gmail.com>
2024-03-14 17:10:20 +1100

    add a modal with nothing inside (yet) for the filtering

---
+++ src/components/FilterModal.jsx
@@ -0,0 +1,24 @@
+import { Modal, Button } from "react-bootstrap";
+
+const FilterModal = ({ showModal, handleClose }) => {
+  return (
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Filters</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Lot of filtering going on here...</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Apply Filters
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default FilterModal;
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,40 +4,46 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import "react-datepicker/dist/react-datepicker.css";
 import Button from "react-bootstrap/Button";
+import FilterModal from "./FilterModal";
 
 function SearchForm() {
   const { setSearchParams } = useSearch();
   const [postcode, setPostcode] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchParams({ postcode });
   };
 
-  const handleFilterClick = () => {};
+  const handleFilterClick = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <InputGroup>
-        <Form.Control
-          type="text"
-          value={postcode}
-          onChange={(e) => setPostcode(e.target.value)}
-          placeholder="Search gig, ..."
-          size="sm"
-        />
-        <Button variant="danger" type="submit" size="sm">
-          Search
-        </Button>
-        <Button
-          variant="outline-secondary"
-          size="sm"
-          onClick={handleFilterClick}
-        >
-          Filters
-        </Button>
-      </InputGroup>
-    </Form>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup>
+          <Form.Control
+            type="text"
+            value={postcode}
+            onChange={(e) => setPostcode(e.target.value)}
+            placeholder="Search gig, ..."
+            size="sm"
+          />
+          <Button variant="danger" type="submit" size="sm">
+            Search
+          </Button>
+          <Button
+            variant="outline-secondary"
+            size="sm"
+            onClick={handleFilterClick}
+          >
+            Filters
+          </Button>
+        </InputGroup>
+      </Form>
+      <FilterModal showModal={showModal} handleClose={handleCloseModal} />
+    </>
   );
 }
 
