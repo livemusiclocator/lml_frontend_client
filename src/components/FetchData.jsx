35645c1106275ad7bb6d37f89f4bc8e1e9875629
Mark Ryall <mark@ryall.name>
2024-03-17 14:54:48 +1000

    use live api endpoint

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -8,21 +8,11 @@ const FetchData = ({ render }) => {
   const { date: selectedDate } = useDate();
 
   useEffect(() => {
-    fetch("https://lml.live/gigs/query")
+    const date = selectedDate.toISOString().slice(0, 10);
+    fetch(`https://lml.live/gigs/query?date_from=${date}&date_to=${date}`)
       .then((response) => response.json())
       .then((data) => {
-        const filteredGigs = data.filter((gig) => {
-          const gigDate = new Date(gig.start_time);
-          const isSameDay =
-            gigDate.getDate() === selectedDate.getDate() && // returns day of the month, 1 to 31
-            gigDate.getMonth() === selectedDate.getMonth() && // returns month, 0 to 11. January gives 0
-            gigDate.getFullYear() === selectedDate.getFullYear();
-          const matchesPostcode = searchParams.postcode
-            ? gig.venue.address.endsWith(searchParams.postcode)
-            : true;
-          return isSameDay && matchesPostcode;
-        });
-        setGigs(filteredGigs);
+        setGigs(data);
       })
       .catch((error) => console.error("Error fetching gigs:", error));
   }, [selectedDate, searchParams.postcode]);
