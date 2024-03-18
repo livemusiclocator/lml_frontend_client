7374317963c2654f84bbbbec8317da17600a97c7
Fred <pf.robert974@gmail.com>
2024-03-19 08:36:13 +1100

    format the date following dd/MM/yyyy

--- src/components/DateSlider.jsx
+++ src/components/DateSlider.jsx
@@ -24,7 +24,7 @@ const DateSlider = ({ date, onChange }) => {
       <DatePicker
         selected={date}
         onChange={handleDateChange}
-        dateFormat="MM/dd/yyyy"
+        dateFormat="dd/MM/yyyy"
         wrapperClassName="date-picker-wrapper"
         withPortal
         portalId="root-portal"
