33be332bec3676cd4b43af369857adbcb20571f2
Luca Cave <lucacave2@gmail.com>
2024-04-13 13:55:29 +1000

    improve tablet and desktop experience

--- src/components/explorer/FiltersAndGigs.jsx
+++ src/components/explorer/FiltersAndGigs.jsx
@@ -7,21 +7,35 @@ import SingleGigDetails from "./SingleGigDetails";
 
 export const FilterWrapper = styled.div`
   position: absolute;
-  bottom: 0;
   z-index: 1000;
   background-color: white;
-  width: 100vw;
   ${(props) => {
     if (!props.$expand) {
       return `
-    border-top-right-radius: 1.5rem;
-    border-top-left-radius: 1.5rem;
-  `;
+      border-top-right-radius: 1.5rem;
+      border-top-left-radius: 1.5rem;
+      `;
     }
   }};
-  height: ${(props) => (props.$expand ? "100%" : "30vh")};
   overflow: hidden;
   transition: height 0.15s ease-in, border-radius 0.15s ease-in;
+  width: 100vw;
+  height: ${(props) => (props.$expand ? "100%" : "30vh")};
+  bottom: 0;
+  @media (min-width: 1024px) {
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+    bottom: 20px;
+    left: 20px;
+    min-width: 300px;
+    max-width: 400px;
+    height: calc(100vh - 20px - 20px);
+    border-bottom-right-radius: 1.5rem;
+    border-bottom-left-radius: 1.5rem;
+
+    #overlay-expand-button {
+      display: none;
+    }
+  }
 `;
 
 export default function FiltersAndGigs({
@@ -62,6 +76,7 @@ export default function FiltersAndGigs({
     <FilterWrapper $expand={expandList}>
       <Link
         to={expandList ? "/" : "/list"}
+        id="overlay-expand-button"
         className="btn btn-sm btn-light w-100 text-center border-bottom rounded-0"
       >
         {expandList ? "Close" : "Expand"}
