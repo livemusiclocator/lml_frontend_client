8a15465a088c32ddaa7e3b66d9664315fb4cb33c
Luca Cave <lucacave2@gmail.com>
2024-04-15 20:48:24 +1000

    set up caddy reverse proxy for local development
    this allows us to use the subdomains that exist in prod on our local machines and therefore test the subdomain specific code we have

---
+++ caddy_up.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+domains=(
+    "lbmf.lml-development.live", 
+    "melbourne.lml-development.live",
+    "sydney.lml-development.live",
+    "brisbane.lml-development.live",
+    "perth.lml-development.live",
+    "adelaide.lml-development.live",
+)
+
+for domain in "${domains[@]}"; do
+    if [[ -z $(cat /etc/hosts | grep -e $domain) ]]; then
+        echo "$domain missing from hosts file, adding now"
+        echo "127.0.0.1 $domain" | sudo tee -a /etc/hosts
+    fi
+done
+
+caddy run
