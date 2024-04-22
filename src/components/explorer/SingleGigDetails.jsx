df3fc80d4b9aeec9fcf956cdd184bf7afee15210
Jen Smith <j@brachychiton.io>
2024-04-23 08:48:17 +1000

    Gig details page with optional image placeholder

--- src/components/explorer/SingleGigDetails.jsx
+++ src/components/explorer/SingleGigDetails.jsx
@@ -1,29 +1,70 @@
-import { Link, useNavigate, useNavigationType } from "react-router-dom";
+import { Link, useNavigationType, useSearchParams } from "react-router-dom";
 import { gigIsSaved, saveGig, unsaveGig } from "../../savedGigs";
+import { LoadingSpinner } from "../loading/LoadingOverlay";
 import { useState } from "react";
 import styled from "styled-components";
+import tw from "tailwind-styled-components";
 
-const StarButton = styled.button`
-  font-size: 1.5em;
-  color: gold !important;
-  padding: 0;
-  border: none;
-  background: none;
+import {
+  ChevronLeftIcon,
+  StarIcon as StarIconSolid,
+  CalendarIcon,
+  MapPinIcon,
+  TicketIcon,
+  ArrowTopRightOnSquareIcon as ExternalLinkIcon,
+} from "@heroicons/react/24/solid";
+import {
+  StarIcon as StarIconOutline,
+  ClockIcon,
+} from "@heroicons/react/24/outline";
+
+const Aside = tw.aside`
+mx-4
+p-6
+flex
+flex-wrap
+gap-4
+justify-center
+
+ *:grow
+*:shrink
+ `;
+
+const EventDetailItem = tw.li`
+  flex items-center space-x-2 font-semibold text-lg
 `;
+const GigHeroImageBanner = () => {
+  // probably a case for some actual css here .
+  return (
+    <div className="relative h-[50vw] min-h-24 min-w-48 max-h-[40svh] overflow-hidden">
+      <div className="absolute left-0 top-0 w-full h-full">
+        <picture>
+          <img
+            src="/placeholder-gig-image.png"
+            alt="Event Hero Image background"
+            className="blur-lg object-fill w-full h-full object-center"
+          />
+        </picture>
+      </div>
+      <div className="relative w-full h-full">
+        <picture>
+          {/* todo: add some other sources and a fallback image  */}
+          <img
+            src="/placeholder-gig-image.png"
+            alt="Event Hero Image"
+            className=" h-full w-full object-contain object-center"
+          />
+        </picture>
+      </div>
+    </div>
+  );
+};
 
-const BackButton = styled.button`
-  font-size: 1.5em;
-  padding: 0;
-  border: none;
-  background: none;
-`;
-
-export default function SingleGigDetails({ gig }) {
+const NavMaybe = ({ gig }) => {
   const [gigSaved, setGigSaved] = useState(gigIsSaved(gig));
 
   const navType = useNavigationType();
   const to = navType === "POP" ? "/" : -1;
-
   const toggleGigSaved = () => {
     if (gigSaved) {
       unsaveGig(gig);
@@ -33,32 +74,141 @@ export default function SingleGigDetails({ gig }) {
       setGigSaved(true);
     }
   };
+  return (
+    <nav className="flex justify-between p-2">
+      <Link to={to}>
+        <ChevronLeftIcon className="size-6" />
+      </Link>
+      <button onClick={toggleGigSaved} className="text-yellow-200">
+        <span className="sr-only">
+          {gigSaved ? "Remove from favourites" : "Add to favourites"}
+        </span>
+        {gigSaved ? (
+          <StarIconSolid className="size-6" />
+        ) : (
+          <StarIconOutline className="size-6" />
+        )}
+      </button>
+    </nav>
+  );
+};
+const GigHeader = ({ gig, className }) => {
+  const [gigSaved, setGigSaved] = useState(gigIsSaved(gig));
+  const navType = useNavigationType();
+  const to = navType === "POP" ? "/" : -1;
+  const toggleGigSaved = () => {
+    if (gigSaved) {
+      unsaveGig(gig);
+      setGigSaved(false);
+    } else {
+      saveGig(gig);
+      setGigSaved(true);
+    }
+  };
+  return (
+    <header className={`flex flex-col ${className || ""}`}>
+      <hgroup className="break-words text-pretty px-4">
+        <p className="font-semibold">
+          <DatetimeDisplay value={gig.date} type="briefDate" />
+        </p>
+        <h4 className="text-4xl font-bold">{gig.name}</h4>
+      </hgroup>
+    </header>
+  );
+};
+
+const ExternalLink = tw.a`text-blue-600 hover:underline visited:text-purple-600 inline-flex items-baseline`;
+
+// todo: Make a nice current-time aware version of this perhaps?
+//
+const DatetimeDisplay = ({ value, start, end, type = "date" }) => {
+  // just a bit of mistakeproofin. probably a bit silly
+  value = value || start;
+  // does undefined use the browser default ?
+  const formatters = {
+    date: new Intl.DateTimeFormat(undefined, {
+      dateStyle: "full",
+    }),
+    briefDate: new Intl.DateTimeFormat(undefined, {
+      month: "long",
+      day: "numeric",
+    }),
+    time: new Intl.DateTimeFormat(undefined, {
+      hour: "numeric",
+      minute: "numeric",
+    }),
+  };
+
+  if (!value) {
+    return null;
+  }
+  const formatter = formatters[type];
+  const display =
+    end && start
+      ? formatter.formatRange(new Date(start), new Date(end))
+      : formatter.format(new Date(value));
+  return <time dateTime={value}>{display}</time>;
+};
+export default function SingleGigDetails({ gig, className, isLoading }) {
+  const [searchParams] = useSearchParams();
+  const showImagePlaceholder = searchParams.get("showImagePlaceholder");
+  if (isLoading || !gig) {
+    // todo: make skeleton load page type thing here to stop the page from jumping about
+    return <LoadingSpinner />;
+  }
 
   return (
-    <>
-      <div
-        className="p-3 border-bottom d-flex justify-content-between"
-        style={{ height: "71px" }}
-      >
-        <Link to={to}>
-          <BackButton className="btn btn-light px-2">←</BackButton>
-        </Link>
-        <StarButton className="px-2 btn btn-light" onClick={toggleGigSaved}>
-          {gigSaved ? "★" : "☆"}
-        </StarButton>
-      </div>
-      <div className="px-3 mt-2">
-        <div className="text-nowrap">
-          {new Date(gig.start_time).toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "numeric",
-            hour12: true,
-          })}
+    <article className={`min-w-40 ${className || ""} pb-4 space-y-4`}>
+      {!showImagePlaceholder && <NavMaybe gig={gig} />}
+      {showImagePlaceholder && <GigHeroImageBanner />}
+      <GigHeader gig={gig} className="max-w-2xl grow shrink-0" />
+      {/* todo: light contrasting colour here or just stick with a grey perhaps? */}
+      <Aside className="bg-gray-200">
+        <div className="flex gap-x-2">
+          <CalendarIcon className="size-6 shrink-0" />
+          <ul className="font-semibold text-lg">
+            <li aria-label="Date">
+              <DatetimeDisplay value={gig.date} />
+            </li>
+            <li aria-label="Time">
+              <DatetimeDisplay
+                start={gig.start_time}
+                end={gig.finish_time}
+                type="time"
+              />
+            </li>
+          </ul>
         </div>
-        <h4>{gig.name}</h4>
-        <p className="mb-1">{gig.venue.name} </p>
-        <p className="text-muted">{gig.venue.address}</p>
-      </div>
-    </>
+
+        <div className="flex gap-x-2">
+          <MapPinIcon className="size-6 shrink-0" />
+          <ul>
+            <li aria-label="Venue" className="font-semibold text-lg">
+              {gig.venue.name}
+            </li>
+            <li aria-label="Venue address">{gig.venue.address}</li>
+            <li aria-label="Directions link">
+              <ExternalLink href="about:blank">
+                Get directions
+                <ExternalLinkIcon className="size-4 self-center mx-1" />
+              </ExternalLink>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-x-2">
+          <TicketIcon className="size-6 shrink-0" />
+
+          <ul>
+            <li className="font-semibold text-lg">Ticket Information</li>
+            <li aria-label="Ticket link">
+              <ExternalLink href={gig.ticketing_url}>
+                Get tickets
+                <ExternalLinkIcon className="size-4 self-center mx-1" />
+              </ExternalLink>
+            </li>
+          </ul>
+        </div>
+      </Aside>
+    </article>
   );
 }
