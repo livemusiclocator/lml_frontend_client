a8cc60498fd4da0e45f612405e9a38016c0a739e
Joe Sustaric <joe8307@gmail.com>
2024-04-28 11:45:20 +1000

    adding in the react-ga4 and init code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import GigExplorer from "./components/GigExplorer";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Layout from "./layouts/default";
+import ReactGA from 'react-ga4';
 // does this just happen automatically?
 import "./index.css";
 import { ThemeProvider } from "styled-components";
 import { getTheme } from "./getLocation";
+
+ReactGA.initialize('G-8TKSCK99CN');
 
 function App() {
   return (
