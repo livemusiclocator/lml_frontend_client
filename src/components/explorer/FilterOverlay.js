29db6136b159d7093261725404727cf975c5d749
Jen Smith <j@brachychiton.io>
2024-05-15 08:56:07 +1000

    Fixed display (on my 13 inch mbp) by maxing out height to h-full not perfect but at least visible

--- src/components/explorer/FilterOverlay.js
+++ src/components/explorer/FilterOverlay.js
@@ -1,7 +1,7 @@
 import tw from "tailwind-styled-components";
 
 export const FilterWrapper = tw.div`
-  z-20 absolute bottom-0 left-0 w-full max-w-2xl p-0 h-full
+  z-20 absolute bottom-0 left-0 w-full max-w-2xl p-0 h-full md:max-h-full
   flex flex-col py-2 overflow-hidden group bg-white
   md:rounded-3xl
   md:shadow-xl
