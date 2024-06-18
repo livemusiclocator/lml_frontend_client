ab09a5c6709b7b855bfd78f53b8da59e6e0d3ffe
Mark Ryall <mark@ryall.name>
2024-06-18 18:17:16 +1000

    change button label

--- src/components/explorer/details/Tickets.jsx
+++ src/components/explorer/details/Tickets.jsx
@@ -7,7 +7,7 @@ export default function Tickets({ url }) {
           className="flex content-center transition items-center justify-center text-center px-8 py-4 text-xl font-medium rounded-md shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 mx-auto px-8"
         >
           <div className="flex items-center justify-start space-x-1.5">
-            <span>Get Tickets</span>
+            <span>Gig Details</span>
           </div>
         </a>
       </section>
