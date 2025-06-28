3c00dc046e6b13a58344ca07c28f8342c8e289c5
Jen Smith <j@brachychiton.io>
2025-06-29 09:32:15 +1000

    request vite to make more predictable filenames for integration/embed purposes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "lml.js", // Single predictable name
+        chunkFileNames: "lml-[name].js",
+      },
+    },
+  },
 });
