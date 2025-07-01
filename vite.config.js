9e3ee76c5e885f7082f58aef78aec3e8f8445d95
Jen Smith <j@brachychiton.io>
2025-07-02 07:55:37 +1000

    configure vite to bundle the css all in one file

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
+function manualChunks(id) {
+  if (id.includes(".css")) {
+    return "styles";
+  }
 
+  return null;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
+        manualChunks: manualChunks,
         entryFileNames: "lml.js", // Single predictable name
         chunkFileNames: "lml-[name].js",
       },
